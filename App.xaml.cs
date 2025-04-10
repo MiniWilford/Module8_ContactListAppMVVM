b715f48f3a55eac898ed06d372cd8e04619f9acf
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:12:13 -0400

    Enabled stacknav for app

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,15 +1,27 @@
-﻿namespace Module8_ContactListAppMVVM
+﻿using System.Collections.ObjectModel;
+using Module8_ContactListAppMVVM.Models;
+using Module8_ContactListAppMVVM.Viewmodels;
+using Module8_ContactListAppMVVM.Views;
+
+namespace Module8_ContactListAppMVVM
 {
     public partial class App : Application
     {
+
+        // Share contact list for every page in the app
+        public static ObservableCollection<Models.Contact> ContactsCollection { get; } = new ObservableCollection<Models.Contact>();
+
+
         public App()
         {
             InitializeComponent();
-        }
+		}
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            // Enable stacknav
+            return new Window(new NavigationPage(new MainPage()));
         }
+
     }
 }
