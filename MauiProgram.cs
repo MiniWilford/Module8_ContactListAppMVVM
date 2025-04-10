a218e39ef37112a0aed61fb91c0012dba7794b8d
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:14:16 -0400

    Added observable collection for contacts to entire app and page services

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,8 @@
 ﻿using Microsoft.Extensions.Logging;
+using Module8_ContactListAppMVVM.Viewmodels;
+using System.Collections.ObjectModel;
+using Module8_ContactListAppMVVM.Views;
+
 
 namespace Module8_ContactListAppMVVM
 {
@@ -14,6 +18,26 @@ namespace Module8_ContactListAppMVVM
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
+
+            // Add ObservableCollection for contacts
+            var sharedContacts = new ObservableCollection<Models.Contact>();
+
+
+            //// Add viewmodels
+            //builder.Services.AddSingleton<MainPageViewModel>(singleton => new MainPageViewModel());
+            //builder.Services.AddSingleton<ContactsPageViewModel>(singleton => new ContactsPageViewModel() 
+            //{
+            //    Contacts = sharedContacts
+            //});
+            //builder.Services.AddTransient<ContactDetailsViewModel>();
+
+
+            // Add Pages
+            builder.Services.AddSingleton<MainPage>();
+            builder.Services.AddSingleton<ContactsPage>();
+            builder.Services.AddTransient<ContactDetailsPage>();
+
+
 
 #if DEBUG
     		builder.Logging.AddDebug();
