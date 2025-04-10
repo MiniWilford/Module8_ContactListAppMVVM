bb3683cc1711c9d2546d91feee71869a1880730a
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:08:00 -0400

    Complete backend logic for contact details

---
+++ Views/ContactDetailsPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Module8_ContactListAppMVVM.Viewmodels;
+
+namespace Module8_ContactListAppMVVM.Views
+{
+    public partial class ContactDetailsPage : ContentPage
+    {
+
+        private ContactDetailsViewModel _contactDetailsViewModel;
+
+        public ContactDetailsPage(Models.Contact contactSelected)
+        {
+            InitializeComponent();
+           
+            // Bind viewmodel & pass contact selected onto page
+            _contactDetailsViewModel = new ContactDetailsViewModel(contactSelected);
+            _contactDetailsViewModel.Navigation = this.Navigation;
+            BindingContext = _contactDetailsViewModel;
+
+        }
+
+	}
+
+}
