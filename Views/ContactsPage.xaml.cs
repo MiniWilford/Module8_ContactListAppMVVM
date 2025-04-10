680b773ea7dabb9a87be91956d2cf7b2f0e34298
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:11:37 -0400

    Complete backend logic

---
+++ Views/ContactsPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Module8_ContactListAppMVVM.Viewmodels;
+
+namespace Module8_ContactListAppMVVM.Views
+{
+    public partial class ContactsPage : ContentPage
+    {
+
+        private ContactsPageViewModel _contactsPageViewModel;
+
+
+        public ContactsPage(ContactsPageViewModel viewmodel)
+        {
+            InitializeComponent();
+
+            // Bind viewmodel and add to navigation stack
+            _contactsPageViewModel = viewmodel;
+            _contactsPageViewModel.Navigation = this.Navigation;
+            BindingContext = _contactsPageViewModel;
+            
+        }
+
+	}
+
+}
