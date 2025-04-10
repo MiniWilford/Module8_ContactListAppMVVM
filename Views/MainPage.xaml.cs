5d6c2db5f901cd989a9ce1ec34bd062c75d1bb62
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:11:54 -0400

    Complete mainpage backend logic

---
+++ Views/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Module8_ContactListAppMVVM.Viewmodels;
+
+namespace Module8_ContactListAppMVVM.Views
+{
+    public partial class MainPage : ContentPage
+    {
+
+        private MainPageViewModel _viewModel;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            // Pass page navigation to viewmodel & ensure to bind viewmodel to BindingContext
+            _viewModel = new MainPageViewModel();
+            _viewModel.Navigation = this.Navigation;
+            BindingContext = _viewModel;
+        }
+    }
+
+}
