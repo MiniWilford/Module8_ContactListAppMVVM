08454a1809e6e6829e383af2d59e8c9e6b11ba70
MiniWilford <45856509+MiniWilford@users.noreply.github.com>
2025-04-10 16:06:46 -0400

    Add viewmodel logic for ContactDetails page

--- Viewmodels/ContactDetailsViewModel.cs
+++ Viewmodels/ContactDetailsViewModel.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
 namespace Module8_ContactListAppMVVM.Viewmodels
 {
-	internal class ContactDetailsViewModel
+	public partial class ContactDetailsViewModel : ObservableObject
 	{
+
+		// Get current navigation stack
+		public INavigation Navigation { get; set; }
+
+
+		[ObservableProperty]
+		private Models.Contact currentContact;
+
+
+		[ObservableProperty]
+		private bool isBeingEdited;  // Track when edits are being made
+
+
+		public ContactDetailsViewModel(Models.Contact contact) 
+		{ 
+			CurrentContact = contact;
+		}
+
+		// Toggle Editable Fields for "Edit" button
+		[RelayCommand]
+		private void EditContact()
+		{
+			IsBeingEdited = true;
+		}
+
+		// Save changes from edit
+		[RelayCommand]
+		private void SaveContactChanges()
+		{
+			IsBeingEdited = false;
+		}
+
+		[RelayCommand]
+		private async void GoBack()
+		{
+			// Return previous page visited to user
+			await Navigation.PopAsync();
+		}
+		
+
 	}
 }
